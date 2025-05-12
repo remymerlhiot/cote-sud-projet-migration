dfeeadb2f1ccd76b64dd7454c778b870c449bdfa
remymerlhiot <merlhiotr@gmail.com>
2025-05-12 14:43:37 +0200

    Delete src directory

--- src/services/wordpress/config.ts
+++
@@ -1,4 +0,0 @@
-
-// Configuration for WordPress API
-export const API_BASE_URL = "https://cote-sud.immo/wp-json/wp/v2";
-export const CUSTOM_API_BASE_URL = "https://cote-sud.immo/wp-json/axo/v1";
--- src/types/window.d.ts
+++
@@ -1,8 +0,0 @@
-
-// Add YouTube API declarations
-interface Window {
-  onYouTubeIframeAPIReady?: () => void;
-  YT?: {
-    Player: any;
-  };
-}
