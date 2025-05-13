1029a0bc303713e79553bb1a4c5ea5b1d318afd6
remymerlhiot <merlhiotr@gmail.com>
2025-05-13 16:16:44 +0200

    Update extractImages.ts

--- src/utils/extractImages.ts
+++ src/utils/extractImages.ts
@@ -1,1 +1,10 @@
+export const extractImagesFromHtml = (html: string): string[] => {
+  if (typeof window === "undefined") return []; // en SSR : sécuriser
 
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const imgTags = Array.from(doc.querySelectorAll("img"));
+  return imgTags
+    .map((img) => img.getAttribute("src"))
+    .filter((src): src is string => Boolean(src));
+};
